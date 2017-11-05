ac57c2ea1c88b64297026f5f90ffae220c442c4b
ciazhar <daisetsunakama@gmail.com>
2017-11-05 15:39:31 +0700

    update model

--- src/main/java/com/ciazhar/springwebfluxcommandpattern/model/mongo/Product.java
+++ src/main/java/com/ciazhar/springwebfluxcommandpattern/model/mongo/Product.java
@@ -2,6 +2,7 @@ package com.ciazhar.springwebfluxcommandpattern.model.mongo;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @Document
 public class Product {
+    @Id
     private String id;
     private String name;
     private Long price;
