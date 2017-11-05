a12358488b6604f8de6dc2c31ccd2d056cf36979
ciazhar <daisetsunakama@gmail.com>
2017-11-05 15:28:21 +0700

    initial commit

---
+++ src/main/java/com/ciazhar/springwebfluxcommandpattern/SpringWebfluxCommandPatternApplication.java
@@ -0,0 +1,12 @@
+package com.ciazhar.springwebfluxcommandpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxCommandPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxCommandPatternApplication.class, args);
+	}
+}
